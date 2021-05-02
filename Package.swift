1c62ef15bcdbf083d98144ee22da14eb499513e4
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-05-02 18:46:06 -0300

    Fixed swifty timer package structure;

--- Package.swift
+++ Package.swift
@@ -1,8 +1,21 @@
+// swift-tools-version: 5.4
+
 import PackageDescription
 
 let package = Package(
 	name: "SwiftyTimer",
-	dependencies: [],
-    exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]
-    
+    platforms: [ .iOS(.v10) ],
+    products: [
+        .library(
+            name: "SwiftyTimer",
+            targets: [
+                "SwiftyTimer"
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftyTimer"
+        )
+    ]
 )
